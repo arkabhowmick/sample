ceb8f27d6ef72bf3c8b60d4961227371fac08a8b
Arka Bhowmick <arka_bhowmick@outlook.com>
2020-02-23 01:51:47 +0530

    Home in progress

--- src/js/home.js
+++ src/js/home.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    adjustCharacterWidth();
+    adjustSpeechBubbleWidth();
+    adjustSpeechBubblePosition();
     var bubbleMoveFlag = true;
     $('#home').find('.speech-bubble-container').on('click', function() {
         if(bubbleMoveFlag) {
@@ -16,4 +19,31 @@ $(document).ready(function() {
         // oldNode.replaceWith(newNode);
         // newNode.addClass('speech-bubble-animation');
     });
+    $(window).resize(function() {
+        adjustCharacterWidth();
+        adjustSpeechBubbleWidth();
+        adjustSpeechBubblePosition();
+    });
+
+    function adjustCharacterWidth() {
+        var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
+        if(windowHeight < windowWidth) {
+            $('.character-container').width(windowHeight);
+        }
+        else {
+            $('.character-container').width(windowWidth);
+        }
+    }
+
+    function adjustSpeechBubbleWidth() {
+        var characterWidth = $('.character-container').width();
+        $('.speech-bubble-container').width((characterWidth*(2/3)) + 'px');
+    }
+
+    function adjustSpeechBubblePosition() {
+        var speechBubbleWidth = $('.speech-bubble-container').width();
+        console.log('Width : ', speechBubbleWidth);
+        $('.speech-bubble-container').css('left', '-' + (speechBubbleWidth/2) + 'px');
+    }
 }); 
---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    new fullpage('#fullpage', {
+        autoScrolling:false,
+        // scrollHorizontally: true
+    });
+    
+    // //methods
+    // fullpage_api.setAllowScrolling(false);
+});
